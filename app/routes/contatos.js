e72a3d2c2d0c56f5f3d8c7b13bc0d2b5350fdfda
Maia <raafamaia@gmail.com>
2015-02-17 21:08:16 -0200

    express-load, contatos, res.json

---
+++ app/routes/contatos.js
@@ -0,0 +1,7 @@
+/**
+ * Created by rafaelmaia on 2/17/15.
+ */
+module.exports = function(app){
+    var controller = app.controllers.contato;
+    app.get('/contatos', controller.listaContatos);
+}
